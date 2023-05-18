9670f80324dc78aaf989913178aa2844f51b2e56
Mario <ggperpi@gmail.com>
2023-05-18 18:39:04 +0200

    EXE unico ok

--- src/main/java/com/mggcode/gestion_bd_elecciones/GestionBdEleccionesApplication.java
+++ src/main/java/com/mggcode/gestion_bd_elecciones/GestionBdEleccionesApplication.java
@@ -6,12 +6,19 @@ import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 
 import java.awt.*;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.nio.file.Paths;
 
 @SpringBootApplication
 public class GestionBdEleccionesApplication {
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_RESET = "\u001B[0m";
+
+
     public static void main(String[] args) {
         SpringApplication.run(GestionBdEleccionesApplication.class, args);
     }
@@ -20,6 +27,27 @@ public class GestionBdEleccionesApplication {
     void applicationReadyEvent() {
         //System.out.println("Abriendo parte gráfica");
         //browse("http://localhost:8080");
+        System.out.println(ANSI_GREEN + "INICIANDO CLIENTE" + ANSI_RESET);
+        runClient();
+    }
+
+    public static void runClient() {
+        String ruta = Paths.get("").toAbsolutePath().toString() + "\\script.bat";
+        try {
+            ProcessBuilder pb = new ProcessBuilder("cmd.exe", "/c", ruta);
+            pb.inheritIO();
+            Process proceso = pb.start();
+
+            int resultado = proceso.waitFor();
+
+            if (resultado == 0) {
+                System.out.println("El archivo .bat se ejecutó correctamente.");
+            } else {
+                System.out.println("Se produjo un error al ejecutar el archivo .bat.");
+            }
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void browse(String url) {
