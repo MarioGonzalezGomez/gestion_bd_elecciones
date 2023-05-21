d779c693200c55c5fd77406b806f3c9812ca348a
Mario <ggperpi@gmail.com>
2023-05-21 18:10:54 +0200

    Cmabio manual entre db

--- src/main/java/com/mggcode/gestion_bd_elecciones/config/AutonomicasDB.java
+++ src/main/java/com/mggcode/gestion_bd_elecciones/config/AutonomicasDB.java
@@ -1,14 +1,18 @@
 package com.mggcode.gestion_bd_elecciones.config;
 
 import jakarta.persistence.EntityManagerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
@@ -29,11 +33,29 @@ public class AutonomicasDB {
     @Value("${autonomicas.jpa.database-platform}")
     private String dialect;
 
+    @Value("${db.nameAuto}")
+    private String dbName;
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    public void changeDataSourceAuto(String newHost) {
+        DataSource dataSource = applicationContext.getBean("autonomicasDatasource", DataSource.class);
+        if (dataSource instanceof SimpleDriverDataSource simpleDataSource) {
+            simpleDataSource.setUrl("jdbc:mysql://" + newHost + ":" + 3306 + "/" + dbName);
+        } else {
+            System.out.println("El dataSource no es una instancia de SimpleDriverDataSource");
+        }
+    }
+
     @Bean(name = "autonomicasDatasource")
     @ConfigurationProperties(prefix = "autonomicas.datasource")
     public DataSource dataSource() {
-        return DataSourceBuilder.create().build();
+        SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+        dataSource.setDriverClass(com.mysql.cj.jdbc.Driver.class);
+        return dataSource;
     }
+
 
     @Bean(name = "autonomicasEntityManagerFactory")
     public LocalContainerEntityManagerFactoryBean entityManagerFactoryBean(@Qualifier("autonomicasDatasource") DataSource dataSource) {
@@ -56,7 +78,7 @@ public class AutonomicasDB {
 
     @Bean
     @Primary
-    @ConfigurationProperties(prefix="autonomicas.datasource")
+    @ConfigurationProperties(prefix = "autonomicas.datasource")
     public DataSource primaryDataSource() {
         return DataSourceBuilder.create().build();
     }
--- src/main/java/com/mggcode/gestion_bd_elecciones/config/MunicipalesDB.java
+++ src/main/java/com/mggcode/gestion_bd_elecciones/config/MunicipalesDB.java
@@ -1,14 +1,18 @@
 package com.mggcode.gestion_bd_elecciones.config;
 
 import jakarta.persistence.EntityManagerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
@@ -29,10 +33,28 @@ public class MunicipalesDB {
     @Value("${municipales.jpa.database-platform}")
     private String dialect;
 
+    @Value("${db.nameMuni}")
+    private String dbName;
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
     @Bean(name = "municipalesDatasource")
     @ConfigurationProperties(prefix = "municipales.datasource")
     public DataSource dataSource() {
-        return DataSourceBuilder.create().build();
+        SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+        dataSource.setDriverClass(com.mysql.cj.jdbc.Driver.class);
+        return dataSource;
+    }
+
+
+    public void changeDataSourceMuni(String newHost) {
+        DataSource dataSource = applicationContext.getBean("municipalesDatasource", DataSource.class);
+        if (dataSource instanceof SimpleDriverDataSource simpleDataSource) {
+            simpleDataSource.setUrl("jdbc:mysql://" + newHost + ":" + 3306 + "/" + dbName);
+        } else {
+            System.out.println("El dataSource no es una instancia de SimpleDriverDataSource");
+        }
     }
 
     @Bean(name = "municipalesEntityManagerFactory")
