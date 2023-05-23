7f693fb454514d1733d2be02ec0c95c880ca0656
Mohamed <moha.as172@gmail.com>
2023-05-23 12:29:49 +0200

    shutdown y parte de los repositorios precompilados

--- src/main/java/com/mggcode/gestion_bd_elecciones/service/autonomicas/ACircunscripcionService.java
+++ src/main/java/com/mggcode/gestion_bd_elecciones/service/autonomicas/ACircunscripcionService.java
@@ -2,9 +2,13 @@ package com.mggcode.gestion_bd_elecciones.service.autonomicas;
 
 import com.mggcode.gestion_bd_elecciones.model.autonomicas.Circunscripcion;
 import com.mggcode.gestion_bd_elecciones.repository.autonomicas.ACircunscripcionRepository;
+import io.debezium.jdbc.JdbcConnection;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -13,7 +17,6 @@ public class ACircunscripcionService implements IBaseService<Circunscripcion> {
 
     @Autowired
     private ACircunscripcionRepository circunscripcionRepository;
-
 
 
     @Override
@@ -28,13 +31,14 @@ public class ACircunscripcionService implements IBaseService<Circunscripcion> {
 
     @Override
     public Circunscripcion findById(String id) {
-        Optional<Circunscripcion> circunscripcionOptional = circunscripcionRepository.findById(id);
-        return circunscripcionOptional.orElse(null);
+
+        Circunscripcion circunscripcionOptional = circunscripcionRepository.findCircunscripcionById(id);
+        return circunscripcionOptional;
     }
 
     @Override
     public List<Circunscripcion> findAll() {
-        return circunscripcionRepository.findAll();
+        return circunscripcionRepository.findAllCircunscripciones();
     }
 
     @Override
